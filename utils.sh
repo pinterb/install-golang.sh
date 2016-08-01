4939d8c40d6d21519005e39c57ec53950740124a
Brad Pinter <brad.pinter@gmail.com>
2016-08-01 12:04:04 -0400

    tweaked utils.sh

--- utils.sh
+++ utils.sh
@@ -7,28 +7,50 @@ readonly DOWNLOAD_URL="https://storage.googleapis.com/golang/go$GOLANG_VERSION.l
 readonly DOWNLOADED_FILE="$DOWNLOAD_DIR/go$GOLANG_VERSION.linux-amd64.tar.gz"
 
 # Get distro data from /etc/os-release
-readonly DISTRO_VER=$(awk -F'=' '/VERSION_ID/ {print $2}' /etc/os-release | tr -d '"')
-readonly DISTRO_ID=$(awk -F'=' '/ID/ {print $2; exit}' /etc/os-release | tr '[:upper:]' '[:lower:]')
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    DISTRO_ID=$DISTRIB_ID
+    DISTRO_VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    DISTRO_ID=Debian
+    DISTRO_VER=$(cat /etc/debian_version)
+elif [ -f /etc/centos-release ]; then
+    DISTRO_ID=$(awk '{print $1}' /etc/centos-release)
+    DISTRO_VER=$(awk '{print $4}' /etc/centos-release)
+elif [ -f /etc/redhat-release ]; then
+    DISTRO_ID=RHEL
+    DISTRO_VER=$(awk '{print $7}' /etc/redhat-release)
+elif [ -f /etc/os-release ]; then
+    DISTRO_ID=$(awk -F'=' '/NAME/ {print $2; exit}' /etc/os-release)
+    DISTRO_VER=$(awk -F'=' '/VERSION_ID/ {print $2}' /etc/os-release | tr -d '"')
+else
+    DISTRO_ID=$(uname -s)
+    DISTRO_VER=$(uname -r)
+fi
 
 
-function warn() {
+readonly TRUE=0
+readonly FALSE=1
+
+
+warn() {
   echo -e "\033[1;33mWARNING: $1\033[0m"
 }
 
-function error() {
+error() {
   echo -e "\033[0;31mERROR: $1\033[0m"
 }
 
-function inf() {
+inf() {
   echo -e "\033[0;32m$1\033[0m"
 }
 
-function follow() {
+follow() {
   inf "Following docker logs now. Ctrl-C to cancel."
   docker logs --follow $1
 }
 
-function run_command() {
+run_command() {
   inf "Running:\n $1"
   eval $1 &> /dev/null
 }
@@ -39,4 +61,16 @@ absolute_path() {
   local abspath="$(pwd -P)"
   popd > /dev/null
   echo "$abspath/$(basename $1)"
+}
+
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+semverParse() {
+  major="${1%%.*}"
+  minor="${1#$major.}"
+  minor="${minor%%.*}"
+  patch="${1#$major.$minor.}"
+  patch="${patch%%[-.]*}"
 }
